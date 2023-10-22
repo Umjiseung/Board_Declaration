5ad934faa32d95cec50fc8b0986b5f96ab9e77ad
Umjiseung <eom071005123@gmail.com>
2023-10-22 19:37:03 +0900

    update::import

--- src/main/java/com/example/demo/domain/board/controller/BoardController.java
+++ src/main/java/com/example/demo/domain/board/controller/BoardController.java
@@ -1,6 +1,10 @@
 package com.example.demo.domain.board.controller;
 
+import com.example.demo.domain.board.data.dto.request.BoardUpdateRequest;
 import com.example.demo.domain.board.data.dto.request.BoardWriteRequest;
+import com.example.demo.domain.board.data.dto.response.BoardGetResponse;
+import com.example.demo.domain.board.data.dto.response.BoardInfoResponse;
+import com.example.demo.domain.board.data.dto.response.BoardUpdateResponse;
 import com.example.demo.domain.board.data.dto.response.BoardWriteResponse;
 import com.example.demo.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/board")
+@RequestMapping("/board")
 public class BoardController {
 
     private final BoardService boardService;
@@ -20,7 +26,7 @@ public class BoardController {
     // 게시물 작성
     @PostMapping("/write")
     public ResponseEntity<BoardWriteResponse> writeBoard(@RequestBody BoardWriteRequest boardCreateRequest) {
-        return new ResponseEntity<>(boardService.boardCreate(boardCreateRequest), HttpStatus.CREATED);
+        return new ResponseEntity<>(boardService.boardWrite(boardCreateRequest), HttpStatus.CREATED);
     }
 
     // 게시물 모두 가져오기
@@ -37,7 +43,7 @@ public class BoardController {
 
     //게시물 수정
     @PutMapping("/{id}")
-    public ResponseEntity<BoardUpdateResponse> updateBoard(@PathVariable("id") Long id ,@RequestBody BoardUpdateRequest boardUpdateRequest) {
+    public ResponseEntity<BoardUpdateResponse> updateBoard(@PathVariable("id") Long id , @RequestBody BoardUpdateRequest boardUpdateRequest) {
         boardService.boardUpdate(boardUpdateRequest, id);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
